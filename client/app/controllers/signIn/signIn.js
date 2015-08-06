5194fcc70e602a72d84678af3203db9086d032d8
Brian Beeler <brianlbeeler@gmail.com>
2015-08-05 22:06:45 -0500

    Annotations

--- client/app/controllers/signIn/signIn.js
+++ client/app/controllers/signIn/signIn.js
@@ -1,10 +1,12 @@
 angular.module('shareAustin')
 
+// Declares a sign controller with user info
 .controller('SignInCtrl', function($scope) {
   $scope.user = {
     userName: '',
     password: ''
   };
+
 
   $scope.submitSignIn = function ($scope) {
     //Post request to server
