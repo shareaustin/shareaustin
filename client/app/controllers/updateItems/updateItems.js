6d94fc5072ed58f2366ca649f3325b009354449d
Brian Beeler <brianlbeeler@gmail.com>
2015-08-07 18:59:55 -0500

    Prepoulated fields on update item

--- client/app/controllers/updateItems/updateItems.js
+++ client/app/controllers/updateItems/updateItems.js
@@ -1,16 +1,17 @@
 angular.module('shareAustin')
 
 .controller('EditItemCtrl', function($scope, Request) {
-  var item_id = 1;
 
   $scope.getItemById = function(itemId) {
-    Request.items.itemById(itemId)
+    Request.items.itemById(itemId).then(function(item) {
+
+      $scope.item = item;
+    })
   },
 
   $scope.editItem = function(item) {
     Request.items.editItem(item);
   };
-
-  var item = $scope.getItemById(1);
-  console.log("Everything worked: " + item)
+  
+  $scope.getItemById(2);
 });
