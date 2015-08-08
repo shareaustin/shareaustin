bea47e3e7b1f5ea77513357c647bb702951cdaac
Hadley Lambeth <hlambeth@gmail.com>
2015-08-07 20:00:38 -0500

    Still working on passport auth

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ var express = require('express');
 var routes = require('./routes.js');
 
 var bodyParser = require('body-parser');
+// var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+
+var User = require('./model/user.js')
 
 var db = require('./db/db.js')
 require('./db/schema.js')();
@@ -20,20 +23,35 @@ app.use (session({
   saveUninitialized: false
 }))
 
-// app.use(passport.initialized());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
+
+//added
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+
 
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../client'));
 
+// !! PROBLEM AREA !!
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
+    new User({
+      'username': username,
+      'password': password
+    }).fetch()
+    .then(function (model) {
+      console.log("Model ", model)
+      return done(null, model);
+    })
+    .catch(function (err) {
       if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.verifyPassword(password)) { return done(null, false); }
-      return done(null, user);
-    });
+      if (!model) { return done(null, false); }
+      if (!model.verifyPassword(password)) {
+        return done(null, false);
+      }
+    })
   }
 ));
 passport.serializeUser(function(user, done) {
@@ -44,9 +62,10 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
-// app.get('/checkAuth', function(req, res, next) {
-//   res.json(req.isAuthenticated());
-// })
+// Not being used yet but would be passed in as a parameter on get post request
+app.get('/checkAuth', function(req, res, next) {
+  res.json(req.isAuthenticated());
+})
 
 // Writes all the routes to the server instance in the routes.js file
 
