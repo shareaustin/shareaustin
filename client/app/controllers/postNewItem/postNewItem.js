fa0181874e923f02578a964e0694bd7ff66d06d6
Brian Beeler <brianlbeeler@gmail.com>
2015-08-10 18:51:47 -0500

    refactor

--- client/app/controllers/postNewItem/postNewItem.js
+++ client/app/controllers/postNewItem/postNewItem.js
@@ -4,17 +4,28 @@ angular.module('shareAustin')
   $scope.newListing = {};
 
   $scope.submitNewListing = function(item) {
+    // Changes user input address to url end-path
     var address = Helpers.urlifyAddress(item.streetAddress);
-    Request.items.getLocation(address).then(function (googleResponse) {
-      var locationInfo = Helpers.simplifyLocation(googleResponse)
-      item.lat       = locationInfo.lat;
-      item.lng       = locationInfo.lng;
-      item.address   = locationInfo.address;
-      item.active    = true;
-      item.available = true;
 
-      Request.items.submitNewListing(item);
-      $scope.item = {};
+    // Request to google maps api for location info 
+    Request.items.getLocation(address)
+      .then(function (googleResponse) {
+
+        // Extracts useful info from google response
+        var locationInfo = Helpers.simplifyLocation(googleResponse)
+
+        // Sets properties not found from form input  
+        item.lat       = locationInfo.lat;
+        item.lng       = locationInfo.lng;
+        item.address   = locationInfo.address;
+        item.active    = true;
+        item.available = true;
+
+        // Add new item to database
+        Request.items.submitNewListing(item);
+
+        // Clear input fields once submitted
+        $scope.item = {};
     })
   };
 
