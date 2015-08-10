c3d081793086571db0be5eb8595de72aea8d9853
Brian Beeler <brianlbeeler@gmail.com>
2015-08-10 18:57:16 -0500

    Location services works with edit item

--- client/app/controllers/updateItems/updateItems.js
+++ client/app/controllers/updateItems/updateItems.js
@@ -9,8 +9,28 @@ angular.module('shareAustin')
   },
 
   $scope.editItem = function(item) {
-    Request.items.editItem(item);
+    // Changes user input address to url end-path
+    var address = Helpers.urlifyAddress(item.streetAddress);
+
+    // Request to google maps api for location info 
+    Request.items.getLocation(address)
+      .then(function (googleResponse) {
+
+        // Extracts useful info from google response
+        var locationInfo = Helpers.simplifyLocation(googleResponse)
+
+        // Sets properties not found from form input  
+        item.lat       = locationInfo.lat;
+        item.lng       = locationInfo.lng;
+        item.address   = locationInfo.address;
+        item.active    = true;
+        item.available = true;
+
+        // Updates item based on new values
+        Request.items.editItem(item)
+    })
   };
-  
+  // Hard coded for now to display item 1
+  // Should display any selected item
   $scope.getItemById(1);
 });
