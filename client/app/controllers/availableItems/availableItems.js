c4d77a8ceee8db1a2fa248dcc30fadbf1d7374ae
Brian Beeler <brianlbeeler@gmail.com>
2015-08-10 18:44:34 -0500

    refactor

--- client/app/controllers/availableItems/availableItems.js
+++ client/app/controllers/availableItems/availableItems.js
@@ -2,79 +2,50 @@ angular.module('shareAustin')
 
 .controller('AvailableItemsCtrl', function($scope, Request, Item) {
 
-  // $scope.item = {
-  //   name: '',
-  //     photo_url: '',
-  //     seller_username: '',
-  //     available: '',
-  //     description: '',
-  //     price_per_hour: '',
-  //     price_per_day: ''
-  // };
   $scope.currentItem = {};
-  $scope.items = [
-  // {
-  //   name: 'Kayak',
-  //     photo_url: 'http://pics.woodenpropeller.com/kayak10.jpg',
-  //     seller_username: 'kayakBob',
-  //     available: 'true',
-  //     description: 'This is a sweet kayak! Please rent it forever! Dog included!',
-  //     price_per_hour: '$10',
-  //     price_per_day: '$40'
-  // },
-  // {
-  //   name: 'Tandem Bike',
-  //     photo_url: 'http://www.atomiczombie.com/plans/tradewinds/TradeWinds%20Recumbent%20Tandem%20Bike%206.jpg',
-  //     seller_username: 'bikerLady',
-  //     available: 'true',
-  //     description: 'Perfect for two people! Warning: do not ride this alone.',
-  //     price_per_hour: '$5',
-  //     price_per_day: '$25'
-  // }
-  ];
+  $scope.items = [];
 
-  $scope.fetchAvailableItems = function() {
-    // Google Map OPtions: Initially center on Texas state capitol building.
+  $scope.setupMap = function() {
+
+    // Austin view centered on capitol building
     var mapOptions = {
       zoom: 13,
       center: new google.maps.LatLng(30.27415, -97.73996),
       mapTypeId: google.maps.MapTypeId.TERRAIN
     }
 
-    // Puts map in container with Id "map"
+    // Places map in container with id 'map'
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions)
 
-    // Fetches all available items
-    Request.items.fetchAvailableItems()    
-    .then(function (results){
-     $scope.items = results;
+    // For each item, place a marker corresponding to its latitude and longitude
+    for (var i = 0; i < $scope.items.length; i++) {
+      // Set coordinates
+      var latLng = new google.maps.LatLng($scope.items[i].lat, $scope.items[i].lng)
+      var markerData = {
+                          position : latLng,
+                          title    : $scope.items[i].name
+                        };
+      // Create marker, set on map
+      var newMark = new google.maps.Marker(markerData)
+      newMark.setMap($scope.map);
+    }
+  }
 
-      // For each item, place a marker corresponding to its latitude and longitude
-      for (var i = 0; i < $scope.items.length; i++) {
-        var latLng = new google.maps.LatLng($scope.items[i].lat, $scope.items[i].lng)
-        console.log(latLng)
-        var newMark = new google.maps.Marker({
-          position: latLng,
-          title: $scope.items[i].name
-        })
-        newMark.setMap($scope.map);
-      }
-    })
+  // Fetches all available items for display
+  $scope.fetchAvailableItems = function() {
+    Request.items.fetchAvailableItems()    
+      .then(function (results){
+        $scope.items = results;
+      })
   };
 
-
-  // $scope.fetchItem = function ($event) {
-  //   Request.items.itemById($event.id).then(function(results) {
-  //     console.log("Current Item is ", results)
-  //     $scope.currentItem = results
-  //     console.log("current item name ", $scope.currentItem.name)
-  //   })
-  // }
+  // Changes item selected for detailed viewing
   $scope.updateItem = function ($event) {
     console.log("Event ", $event)
     Item.set($event)
   }
 
+  // Immediately invoked with page
   $scope.fetchAvailableItems();
-  // $scope.fetchItem(1)
+  $scope.setupMap();
 })
