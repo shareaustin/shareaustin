8db90b8b78f2e88471735ae715afe1c35ab7f1cc
Brian Beeler <brianlbeeler@gmail.com>
2015-08-11 15:57:17 -0500

    Routing from map working

--- client/app/controllers/availableItems/availableItems.js
+++ client/app/controllers/availableItems/availableItems.js
@@ -1,6 +1,6 @@
 angular.module('shareAustin')
 
-.controller('AvailableItemsCtrl', function($scope, $location, Request, Item) {
+.controller('AvailableItemsCtrl', function($scope, $window, $location, $window, Request, Item) {
 
   $scope.currentItem = {};
   $scope.items = [];
@@ -18,37 +18,29 @@ angular.module('shareAustin')
     // Places map in container with id 'map'
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions)
 
-    var storage = {};
     // For each item, place a marker corresponding to its latitude and longitude
     for (var i = 0; i < $scope.items.length; i++) {
-     
+      
       var latLng = new google.maps.LatLng($scope.items[i].lat, $scope.items[i].lng)
       var markerSettings = {
                               position : latLng,
                               map      : $scope.map,
-                              };
+                              item     : $scope.items[i]
+                            };
+      
       var newMark   = new google.maps.Marker(markerSettings)
       var setEvent = google.maps.event.addListener;
 
-      storage[i] = {};
-      storage[i].item = $scope.items.length;
-      storage[i].mark = newMark;
-
       //Event handlers for icons
-      setEvent(storage[i].mark, 'mouseover', function(event) {
-        console.log(event)
+      setEvent(newMark, 'mouseover', function(event) {
         this.setIcon("http://drdeclutterblog.com/wp-content/uploads/2011/08/canoe.thumbnail.JPG");
       });
-      setEvent(storage[i].mark, 'mouseout',  function(event) {
+      setEvent(newMark, 'mouseout', function(event) {
         this.setIcon(); // Passing in nothing changes icon to default
       });
-      setEvent(storage[i].mark, 'click', function(event)  {
-        $scope.updateItem(storage[i].item)
-        $location.path('/item-description')
+      setEvent(newMark, 'click', function(event)  {
+        $scope.updateItem(this.item);
       });
-
-
-      //newMark.setMap($scope.map);
     }
   }
 
@@ -57,18 +49,16 @@ angular.module('shareAustin')
     Request.items.fetchAvailableItems()    
       .then(function (results){
         $scope.items = results;
-
         // Setup map AFTER allitems have been fetched
         $scope.setupMap();
       })
   };
-
   // Changes item selected for detailed viewing
   $scope.updateItem = function ($event) {
     console.log("Event ", $event)
     Item.set($event)
+    $location.path('/item-description');
   }
-
   // Immediately invoked with page
   $scope.fetchAvailableItems()
 })
