c789a624fdf5dc7e60e2867b4aa5222f08605d13
Hadley Lambeth <hlambeth@gmail.com>
2015-08-11 12:11:42 -0500

    setup directive for slider working on slider functionality

--- client/app/directives/slider/slider.js
+++ client/app/directives/slider/slider.js
@@ -1,27 +1,28 @@
 angular.module('shareAustin')
 .controller('SliderCtrl', function ($scope) {
-  $scope.myInterval = 5000;
+  $scope.myInterval = 10;
   $scope.noWrapSlides = false;
-  var slides = $scope.slides = [
-    {image: 'http://lorempixel.com/400/200/'},
-    {image: 'http://www.lorempixel.com/400/200/'}
+  $scope.slides = [
+    {image: 'http://www.paddleboston.com/rentals/images/canoe.jpg'},
+    {image: 'http://www.jonsbushcraft.com/images/canoe2/canoe14.jpg'}
   ];
-  $scope.addSlide = function() {
-    var newWidth = 600 + slides.length + 1;
-    // slides.push({
-    //   image: '//placekitten.com/' + newWidth + '/300',
-    //   text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-    //     ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-    // });
-  };
-  for (var i=0; i<4; i++) {
-    $scope.addSlide();
-  }
+  // $scope.addSlide = function() {
+  //   var newWidth = 600 + $scope.slides.length + 1;
+  //   // slides.push({
+  //   //   image: '//placekitten.com/' + newWidth + '/300',
+  //   //   text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+  //   //     ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+  //   // });
+  // };
+  // for (var i=0; i<4; i++) {
+  //   $scope.addSlide();
+  // }
 })
 .directive('slider', function () {
   return {
     restrict: "E",
-    templateUrl: "directives/slider/slider.html",
+    templateUrl: "app/directives/slider/slider.html",
+    controller: 'SliderCtrl',
     scope: {}
   }
 })
