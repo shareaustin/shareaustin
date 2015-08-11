42b7c283aef9723e4a5c0223c930577c73c0a668
Brian Beeler <brianlbeeler@gmail.com>
2015-08-11 14:34:28 -0500

    Unique event handling for markers

--- client/app/controllers/availableItems/availableItems.js
+++ client/app/controllers/availableItems/availableItems.js
@@ -1,6 +1,6 @@
 angular.module('shareAustin')
 
-.controller('AvailableItemsCtrl', function($scope, Request, Item) {
+.controller('AvailableItemsCtrl', function($scope, $location, Request, Item) {
 
   $scope.currentItem = {};
   $scope.items = [];
@@ -18,26 +18,35 @@ angular.module('shareAustin')
     // Places map in container with id 'map'
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions)
 
+    var storage = {};
     // For each item, place a marker corresponding to its latitude and longitude
     for (var i = 0; i < $scope.items.length; i++) {
-      console.log("for loop")
-      // Create marker, set on map
+     
       var latLng = new google.maps.LatLng($scope.items[i].lat, $scope.items[i].lng)
       var markerSettings = {
                               position : latLng,
                               map      : $scope.map,
-                              //icon     : "http://drdeclutterblog.com/wp-content/uploads/2011/08/canoe.thumbnail.JPG",
-                            };
+                              };
       var newMark   = new google.maps.Marker(markerSettings)
       var setEvent = google.maps.event.addListener;
 
-      setEvent(newMark, 'mouseover', function(event) {
-        newMark.setIcon("http://drdeclutterblog.com/wp-content/uploads/2011/08/canoe.thumbnail.JPG");
+      storage[i] = {};
+      storage[i].item = $scope.items.length;
+      storage[i].mark = newMark;
+
+      //Event handlers for icons
+      setEvent(storage[i].mark, 'mouseover', function(event) {
+        console.log(event)
+        this.setIcon("http://drdeclutterblog.com/wp-content/uploads/2011/08/canoe.thumbnail.JPG");
       });
-      setEvent(newMark, 'mouseout', function(event) {}
-        newMark.setIcon();
+      setEvent(storage[i].mark, 'mouseout',  function(event) {
+        this.setIcon(); // Passing in nothing changes icon to default
       });
-      
+      setEvent(storage[i].mark, 'click', function(event)  {
+        $scope.updateItem(storage[i].item)
+        $location.path('/item-description')
+      });
+
 
       //newMark.setMap($scope.map);
     }
