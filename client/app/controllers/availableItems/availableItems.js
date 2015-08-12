3e9bea603c527f54d972ca22ed308c0d55398bca
Braden Walker <wwbraden@gmail.com>
2015-08-12 14:30:45 -0500

    fixes redirect error

--- client/app/controllers/availableItems/availableItems.js
+++ client/app/controllers/availableItems/availableItems.js
@@ -74,7 +74,7 @@ angular.module('shareAustin')
   $scope.updateItem = function ($event) {
     console.log("Event ", $event)
     Item.set($event)
-    $location.path('../item-description');
+    $location.path('/item-description');
   }
   // Immediately invoked with page
   $scope.newFavorite = function ($event) {
