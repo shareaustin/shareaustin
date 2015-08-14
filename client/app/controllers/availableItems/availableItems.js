3023f906c314a30ddb3975ef0d400ad66a77ef9d
Brian Beeler <brianlbeeler@gmail.com>
2015-08-13 21:41:33 -0500

    Split available items and googlemaps.js

--- client/app/controllers/availableItems/availableItems.js
+++ client/app/controllers/availableItems/availableItems.js
@@ -1,16 +1,21 @@
 angular.module('shareAustin')
 
 .controller('AvailableItemsCtrl', function($scope, $window, $location, $window, Request, Helpers, Item) {
+  
+  // Allows this controller to be expanded into googlemMap.js file
+  angular.module('shareAustin').expandAvailableItems($scope, Request, Helpers)  
 
   // Initialize containers for data
   $scope.currentItem = {};
-  $scope.items = [];
-  $scope.fav = {}
+  $scope.items       = [];
+  $scope.fav         = {};
 
   // Fetches all available items for display; sets up map with these items; 
   $scope.loadPage = function() {
     Request.items.fetchAvailableItems()
       .then(function (results){
+        console.log("B")
+        console.log(results)
         $scope.items = results;
         $scope.setupMap();
       })
@@ -30,71 +35,14 @@ angular.module('shareAustin')
     $location.path('/item-description');
   }
 
-  // Sets google map display, zoomed on Austin;
-  // Loops through scope.items, creating pins for each item;
-  // Sets upp hover, mouseout, and click events.
-  $scope.setupMap = function() {
-
-    var mapSettings = {
-      zoom      : 13,
-      center    : new google.maps.LatLng(30.27415, -97.73996),
-      MapTypeId : google.maps.MapTypeId.TERRAIN
-    }
-
-    // Creates map and places it in the div with id 'map'
-    $scope.map = new google.maps.Map(document.getElementById('map'), mapSettings)
-
-    // Creates a google map pin/marker for each item in scope
-    for (var i = 0; i < $scope.items.length; i++) { 
-      // Latitude & Longitude
-      var latLng = new google.maps.LatLng($scope.items[i].lat, $scope.items[i].lng)
-      // Note: item is not a googlemap property; stored in marker for our convenience.
-      var markerSettings = {
-                              position : latLng,
-                              map      : $scope.map,
-                              item     : $scope.items[i],
-                            };                      
-      var newMark   = new google.maps.Marker(markerSettings)
-
-      // Note: stores google maps addListener FUNCTION to a shorter variable name
-      var setEvent = google.maps.event.addListener;
-
-      // Mouseover marker event
-      setEvent(newMark, 'mouseover', function(event) {
-        var customHtml    = Helpers.createHTMLStr(this.item.name, this.item.price_per_day, this.item.photo_url)
-        // Save info window to global scope, for use in mouseout event
-        window.infoWindow = new google.maps.InfoWindow({content : customHtml, 
-                                                        maxWidth: 150         })
-        infoWindow.open($scope.map, this) //this refers to the marker that was clicked
-      })
-        
-      });
-
-      // Event when mouse leaves the marker
-      setEvent(newMark, 'mouseout', function(event) {
-        
-        // Close the info window
-        window.infoWindow.close();
-      });
-
-      // On click, load the corresponding item to details page
-      // and navigate to details page
-      setEvent(newMark, 'click', function(event)  {
-        // See line #
-        $scope.loadDetailedView(this.item);
-      });
-    } //end for loop
-  } // end function setupMap
-
-
   // Immediately invoked with page
   $scope.newFavorite = function ($event) {
 
-    // Set new favorite with item id, and userId
-    $scope.fav.item_id = $event.id
-    $scope.fav.user_id = Auth.user().id
+    // Sets new favorite with item id, and userId
+    $scope.fav.item_id =  $event.id;
+    $scope.fav.user_id =  Auth.user().id;
 
-    // Post this favorite to database
+    // Posts this favorite to database
     Request.favorites.addFavorite($scope.fav)
   }
 
