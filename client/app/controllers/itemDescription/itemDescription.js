652f99f17df68a8a69b38d9633ec9a179adc7097
Hadley Lambeth <hlambeth@gmail.com>
2015-08-19 14:00:18 -0500

    updated item view to show seller ratings and fixed seller rating in database

--- client/app/controllers/itemDescription/itemDescription.js
+++ client/app/controllers/itemDescription/itemDescription.js
@@ -1,8 +1,17 @@
 angular.module('shareAustin')
 .controller('ItemDescriptionCtrl', function ($scope, $location, Item, Request, CalEvents) {
   $scope.item = Item.get();
+  console.log($scope.item)
+  $scope.availItems = Request.items.fetchAvailableItems()
 
   $scope.rentRedirect = function () {
     $location.path('/transaction')
   }
+
+  $scope.sellerReviews = $scope.item.seller.soldTransactions
+    .filter(function (trans) {
+      return trans.rating && trans.rating.seller_review
+    }).map(function (transaction) {
+      return transaction.rating.seller_review
+    })
 })
