e5724e530a30a69c5e797508076b9a8dfc991d58
dtc3ce <dtc3ce@virginia.edu>
2015-08-19 16:37:01 -0500

    attach users and item to chats

--- server/model/chat.js
+++ server/model/chat.js
@@ -3,8 +3,21 @@ var Chat= bookshelf.Model.extend({
 	tableName: 'chats',
   //idAttribute: ['item_id', 'buyer_id'],
   
+  item: function(){
+    return this.belongsTo('Item')
+  },
+
   messages: function(){
     return this.hasMany('Message');
+  },
+
+  buyer: function(){
+    return this.belongsTo('User', 'buyer_id')      
+  },
+
+  seller: function(){
+    return this.belongsTo('User', 'seller_id')
+    .through('Item')  
   },
 });
 
