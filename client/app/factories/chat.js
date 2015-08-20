33b307624ec5988611606410980e343e8a9556bd
dtc3ce <dtc3ce@virginia.edu>
2015-08-19 19:45:32 -0500

    client join current or creates new chat

--- client/app/factories/chat.js
+++ client/app/factories/chat.js
@@ -19,9 +19,19 @@ angular.module('shareAustin')
       return resp.data;
     });
   };
+
+  function joinOrCreate(data){
+    return $http({
+      method: "POST",
+      url: "/api/chats/find-or-create",
+      data: data,
+    })
+  };
+
   return {
     getRoom: getRoom,
     setRoom: setRoom,
     userChats: userChats,
+    joinOrCreate: joinOrCreate,
   }
 })
--- server/requestHandler/chatHandler.js
+++ server/requestHandler/chatHandler.js
@@ -10,4 +10,17 @@ module.exports = {
       res.json(chat);
     }) 
   },
+
+  findOrCreate: function(req, res){
+    new Chat({
+      item_id: req.body.item_id,
+      buyer_id: req.body.buyer_id,
+    }).save().then(function(chat){
+      console.log('----created new chat\n')
+      res.json(chat);
+    }).catch(function(err){
+      console.log('*******chat already exists\n')
+      res.json(req.body);
+    })                 
+  },
 };
