908c9a27d206f2bae50ff226923b31d3dc3d63ab
Brian Beeler <brianlbeeler@gmail.com>
2015-08-21 21:31:51 -0500

    more comments in auth foler

--- client/app/controllers/auth/auth.js
+++ client/app/controllers/auth/auth.js
@@ -2,22 +2,28 @@ angular.module('shareAustin')
 
 .controller('AuthCtrl', function inject($scope, Auth){
 	$scope.active = 'logIn'
-	$scope.change = function (status) {
+	// Determines which tab is selected
+  $scope.change = function (status) {
 		if (status === 'logIn') $scope.active = true
 		if (status === 'signUp') $scope.active = false
 	};
 })
+// Sign up controller
 .controller('SignUpCtrl', function inject($scope, $location, Auth, sweet){
 	$scope.submitSignUp = function(user){
-		Auth.signup(user)
+		// Request goes thru passport to create a user if valid
+    Auth.signup(user)
 		.then(function(user){
+        // Sets user in Auth factory to be used in other files
 				Auth.setUser(user);
+        // Change location to dashboard
 				$location.path('/dashboard')
-
 		})
+    // Error Handling
 		.catch(function(err){
 			console.log(err)
-			sweet.show({
+			// Popup which shows errors with improper entry
+      sweet.show({
             title: "<small>Error</small>",
             text: '<p>' + err.data.substr(6, err.data.indexOf('<') - 1) + '</p>',
             type: 'error',
@@ -25,20 +31,24 @@ angular.module('shareAustin')
         });
 			// $location.path('/auth')
 		});
-
 	}
 })
+// Sign in controller
 .controller('SignInCtrl', function inject($scope, $location, Auth, sweet){
 	$scope.submitSignIn = function(user){
-		Auth.signin(user)
+		// Passport user validation
+    Auth.signin(user)
 		.then(function(user){
         console.log("successful signin response: ", user);
-				Auth.setUser(user);
+				// Set user in factory for use in other files
+        Auth.setUser(user);
+        // Brings up dashboard view after succesful sign in
 				$location.path('/dashboard')
-
 		})
+    // Error handling
 		.catch(function(err){
 			console.log(err)
+      // Popup showing password/username errors
 			sweet.show({
             title: "<small>Error</small>",
             text: '<p>' + err.data.substr(6, err.data.indexOf('<') - 1) + '</p>',
