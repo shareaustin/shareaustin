3eccad9d046867cce88551626ccfa283daac7eb8
Brian Beeler <brianlbeeler@gmail.com>
2015-08-22 09:41:10 -0500

    Comments in feedBack folder

--- client/app/controllers/feedBack/feedBack.js
+++ client/app/controllers/feedBack/feedBack.js
@@ -83,7 +83,7 @@ angular.module('shareAustin')
   $scope.chooseFour  = function() { clickStar(4) }
   $scope.chooseFive  = function() { clickStar(5) }
 
-
+// Fetches ratings, in case we are updating an existing rating
 $scope.getRating = function() {
   Request.ratings.fetchRating($scope.transaction.id).then(function(response) {
   if (response.data === null) { 
@@ -98,48 +98,56 @@ $scope.getRating = function() {
   })
 }
 
+// Fetches rating associated with this transaction
 $scope.getRating($scope.transaction.id);
 
+// Creates a new rating in database, unless there is already one
+// associated with the transaction; if there already is on, 
+// updates that rating in db
 $scope.submitRatingAndReview = function() { 
 
-
-  
+  // This object is what will be sent to the db
   ratingAndReview = {}
+
+  // Give it properties associated with transaction
   ratingAndReview.transaction_id = $scope.transaction.id;
   ratingAndReview.item_id        = $scope.transaction.item_id;
 
+  // Transaction status also changes with rating submission. Initialized here.
   trsnUpdate = { id: $scope.transaction.id };
 
+  // Evaluates to boolean - true if a buyer is submitting the rating, false if a seller is
   var buyerSubmission = ($scope.transaction.buyer_id === $scope.user.id)
 
+  // Buyers write seller ratings and reviews, so update these field if buyer
   if (buyerSubmission) {
     ratingAndReview.seller_rating = $scope.userInput.rating;
     ratingAndReview.seller_review = $scope.userInput.review + "\n-" + $scope.user.first_name+" "+$scope.user.last_name; 
-
   }
+  // Sellers write buyer ratings and reviews, so update these if seller
   else {
     ratingAndReview.buyer_rating  = $scope.userInput.rating;
     ratingAndReview.buyer_review  = $scope.userInput.review + "\n-" + $scope.user.first_name+" "+$scope.user.last_name;
   }
-
+  // If there is no rating associated with this transaction, this is a new rating, so use addRating request
   if ($scope.newRating) {
     console.log("Brand new rating")
     Request.ratings.addRating(ratingAndReview);
 
-    // if buyer submission update transaction status to seller review done
+    // if buyer submission, status changes status of transaction to "rating from seller pending"
     if (buyerSubmission) {
       trsnUpdate.status = 'rating from seller pending'
       Request.items.updateTransaction(trsnUpdate)
-      $location.path("/dashboard")
+      $location.path("/dashboard") // go to dashboard
     }
+    // if seller submission, change status to show a buyer rating is still needed
     else {
       trsnUpdate.status = 'rating from buyer pending'
       Request.items.updateTransaction(trsnUpdate)
-      $location.path("/dashboard")
+      $location.path("/dashboard") // go to dahsboard
     }
-    // if not   update transaction status to buyer review done
-    
   }
+  // If not a new rating update the existing rating, and change transaction status to complete
   else {
     console.log("update Existing rating")
     trsnUpdate.status = "complete";
