5d13e6dab0db2d5e34c642a5a89b99d7bac89c57
Braden Walker <wwbraden@gmail.com>
2015-08-21 19:45:23 -0500

    heroku db settings test 1

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,25 @@
-var knex = require('knex')({
-  client: 'postgres',
-  connection: {
-  	client: 'pg',
-    connection: DATABASE_URL
-  }
-})
+var Knex = require('knex');
+var URL = require('url');
+var pg_server = URL.parse(process.env.DATABASE_URL);
+var knex = Knex({
+    client: 'pg',
+    connection: {
+        host: pg_server.hostname,
+        port: pg_server.port,
+        user: pg_server.auth.split(':')[0],
+        password: pg_server.auth.split(':')[1],
+        database: pg_server.path.substring(1),
+        ssl: true
+    }
+});
+
+// var knex = require('knex')({
+//   client: 'postgres',
+//   connection: {
+//   	client: 'pg',
+//     connection: DATABASE_URL
+//   }
+// })
 
 var db = require('bookshelf')(knex);
 db.plugin('registry');
