f46b135f7f820588402736e6514dea9917b7b599
Brian Beeler <brianlbeeler@gmail.com>
2015-08-22 12:09:23 -0500

    Add comments to postNewPhoto page

--- client/app/controllers/postNewItem/postNewItem.js
+++ client/app/controllers/postNewItem/postNewItem.js
@@ -1,23 +1,26 @@
 angular.module('shareAustin')
 
+// Controller for posting a new item
 .controller('PostNewItemCtrl', function ($scope, $location, Auth, Item, Request, Helpers) {
+  // Initialize the new Listing
   $scope.newListing = {};
 
+  // Post user input as new listing in database
   $scope.submitNewListing = function(item) {
+    
     // Changes the user inputted address to a url end-path
     var address = Helpers.urlifyAddress(item.address);
-
+    
     // Request to google maps api for location info 
     Request.items.getLocation(address)
       .then(function (googleResponse) {
-
+        
         // Extracts useful info from google response
         var locationInfo = Helpers.simplifyLocation(googleResponse)
         console.log(locationInfo)
 
-        // Sets properties not found from form input  
+        // Sets properties of the listing not attained from form input  
         item.seller_id = Auth.getUser() ? Auth.getUser().id : 1;
-        console.log("expect this = 1: " + item.seller_id)
         item.lat       = locationInfo.lat;
         item.lng       = locationInfo.lng;
         item.address   = locationInfo.address;
@@ -28,59 +31,49 @@ angular.module('shareAustin')
         Request.items.submitNewListing(item)
         .then(function (item){
           Item.set(item);
-          $location.path('/item-primary-photo')
-          
+          $location.path('/item-primary-photo')  
         })
-
 
         // Clear input fields once submitted
         $scope.item = {};
     })
   };
-
-  $scope.addPhoto = function() {
-    console.log('hey!!')
-  };
 })
+// Controller for adding photos to the item
 .controller('ItemPrimaryPhotoCtrl', function ($scope, $location, Item, Request, sweet){
+  
+  // Getter/setter returns boolean
   $scope.photoStatus = function() {
     return Item.getPhotoStatus();
   };
 
+  // Gets the item we are adding pics to
   $scope.item = Item.get();
+
+  // Executes when drag and drop or select a photo to be uplaoded
   $scope.$watch('files', function(){
     $scope.uploadPhoto($scope.files, $scope.item);
-    // $scope.photoStatus();
   })
 
+  // User can only click submit is this status is true
+  // Used to prevent changing location during upload
   Item.setPhotoStatus(true);
   console.log("Item.getPhotoStatus() ", Item.getPhotoStatus());
   console.log($scope.photoStatus());
 
-
+  // Uploads an array of files, set photoStatus to false
   $scope.uploadPhoto = function(files, item){
-    // $scope.photoStatus = true;
     if (files && files.length){
       Item.setPhotoStatus(false);
       files.forEach(Item.uploadPhoto.bind(null, item.id));
-      //Request.items.editItem(Item.get())
     }
-    //$scope.primaryPhotoUrl = Item.getPrimaryPhotoUrl();
-    //console.log(Item.get());
   }
-
-   $scope.update = function(){
+  // Brings user to dashboard (unless upload in process)
+  $scope.update = function(){
     if (Item.getPhotoStatus() === true) {
-     // $scope.photoStatus = false;
-     // console.log("$scope.photoStatus: ", $scope.photoStatus);
-     // console.log("Item photo uploaded. Redirecting to dashboard.");
-     Request.items.editItem(Item.get())
-
-     // .then(function(resp){
-     //  console.log("Success response from cloudinary: ", resp.data);
-     // })
-     $location.path('/dashboard')
-     // Item.setPhotoStatus(false);
+      // Updates appropriate item and changes path to dashboard
+      Request.items.editItem(Item.get())
+      $location.path('/dashboard');
     } else {
       console.log("Item photo not yet uploaded.");
     }
