ea4d70aef830649c5ae9839522aef7acbe07be95
dtc3ce <dtc3ce@virginia.edu>
2015-08-24 15:18:16 -0500

    handled merge conflicts

--- client/app/controllers/auth/auth.js
+++ client/app/controllers/auth/auth.js
@@ -8,26 +8,17 @@ angular.module('shareAustin')
 		if (status === 'signUp') $scope.active = false
 	};
 })
-<<<<<<< HEAD
 // Sign up controller
 .controller('SignUpCtrl', function inject($scope, $location, Auth, sweet){
-=======
-.controller('SignUpCtrl', function inject($scope, $state, $location, Auth, sweet){
->>>>>>> user always loads in dashboard
 	$scope.submitSignUp = function(user){
 		// Request goes thru passport to create a user if valid
     Auth.signup(user)
 		.then(function(user){
         // Sets user in Auth factory to be used in other files
 				Auth.setUser(user);
-<<<<<<< HEAD
         // Change location to dashboard
 				$location.path('/dashboard')
-=======
-				//$location.path('/dashboard')
-        $state.go('dashboard.currentListings');
 
->>>>>>> user always loads in dashboard
 		})
     // Error Handling
 		.catch(function(err){
@@ -43,29 +34,17 @@ angular.module('shareAustin')
 		});
 	}
 })
-<<<<<<< HEAD
 // Sign in controller
 .controller('SignInCtrl', function inject($scope, $location, Auth, sweet){
-=======
-.controller('SignInCtrl', function inject($scope, $location, $state, Auth, sweet){
->>>>>>> user always loads in dashboard
 	$scope.submitSignIn = function(user){
 		// Passport user validation
     Auth.signin(user)
 		.then(function(user){
-<<<<<<< HEAD
-        console.log("successful signin response: ", user);
+        //console.log("successful signin response: ", user);
 				// Set user in factory for use in other files
         Auth.setUser(user);
         // Brings up dashboard view after succesful sign in
 				$location.path('/dashboard')
-=======
-//        console.log("successful signin response: ", user);
-				Auth.setUser(user);
-				//$location.path('/dashboard')
-        $state.go('dashboard.currentListings');
-
->>>>>>> user always loads in dashboard
 		})
     // Error handling
 		.catch(function(err){
