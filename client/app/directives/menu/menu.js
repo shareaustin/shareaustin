76736f76766814b8015ba68b4ca4aa074c5448eb
Hadley Lambeth <hlambeth@gmail.com>
2015-08-25 17:26:40 -0500

    added toggle to hamburger menu

--- client/app/directives/menu/menu.js
+++ client/app/directives/menu/menu.js
@@ -1,6 +1,13 @@
 angular.module('shareAustin')
 .controller('MenuCtrl', function ($scope) {
-
+  $scope.hamburgerStatus = false;
+  $scope.hamburgerToggle = function () {
+    $scope.hamburgerStatus = ($scope.hamburgerStatus === false) ? true : false
+    console.log($scope.hamburgerStatus)
+  }
+// when you click on hamburger menu
+// change ul class name to adjust styling width 100%
+// change ul li class name to be display block, text-align center
 })
 .directive('menu', function () {
   return {
