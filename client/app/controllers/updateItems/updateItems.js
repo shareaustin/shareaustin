1c0d9dd57163ad24ae60df99f43a8cae95dff723
Hadley Lambeth <hlambeth@gmail.com>
2015-08-26 11:36:25 -0500

    updated edit item submit to redirect to dashboard

--- client/app/controllers/updateItems/updateItems.js
+++ client/app/controllers/updateItems/updateItems.js
@@ -1,13 +1,13 @@
 angular.module('shareAustin')
 
-.controller('EditItemCtrl', function ($scope, Helpers, Item, Request) {
+.controller('EditItemCtrl', function ($scope, $location, Helpers, Item, Request) {
 
   // Runs if files are changed
   $scope.$watch('files', function(){
     $scope.uploadPhoto($scope.files, $scope.item);
   });
-  
-  // Fetches an item based on id 
+
+  // Fetches an item based on id
   $scope.getItemById = function(itemId) {
     Request.items.itemById(itemId).then(function(item) {
       console.log("getItembyId returns:")
@@ -21,14 +21,14 @@ angular.module('shareAustin')
     // Changes user input address to url end-path
     var address = Helpers.urlifyAddress(item.address);
 
-    // Request to google maps api for location info 
+    // Request to google maps api for location info
     Request.items.getLocation(address)
       .then(function (googleResponse) {
 
         // Extracts useful info from google response
         var locationInfo = Helpers.simplifyLocation(googleResponse)
 
-        // Sets properties not found from form input  
+        // Sets properties not found from form input
         item.lat       = locationInfo.lat;
         item.lng       = locationInfo.lng;
         item.address   = locationInfo.address;
@@ -37,6 +37,7 @@ angular.module('shareAustin')
 
         // Updates item based on new values
         Request.items.editItem(item)
+        $location.path('/dashboard')
     })
   };
 
