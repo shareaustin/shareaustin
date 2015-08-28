0d225c98603ae79c0d082ae5f0b6e0f05c4716b1
Brian Beeler <brian.beeler@uky.edu>
2015-08-28 12:11:33 -0500

    Update home.js

--- client/app/controllers/home/home.js
+++ client/app/controllers/home/home.js
@@ -1,5 +1,5 @@
 angular.module('shareAustin')
-.controller ('HomeCtrl', function ($scope, $location, Item, Auth) {
+.controller ('HomeCtrl', function ($scope, $location, Item) {
   $scope.search = ""
   $scope.sendSearch = function (term) {
     // Sends search data to all-listings, and gos to that path
