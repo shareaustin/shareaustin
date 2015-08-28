a3ae9feee5864aa965978820669dc2eef0993fc0
Braden Walker <wwbraden@gmail.com>
2015-08-28 11:46:21 -0500

    test gulp automation

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp       = require("gulp");
+var gutil      = require("gulp-util");
+var sass       = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+
+gulp.task("default", function() {
+  return gutil.log("Gulp is running!");
+});
+
+gulp.task("build-css", function() {
+  return gulp.src("./scss/*.scss")
+  .pipe(sourcemaps().init())
+  .pipe(sass())
+  .pipe(sourcemaps().write())
+  .pipe(gulp.dest("./client/css/"));
+});
+
+gulp.task("watch", function() {
+  return gulp.watch("./scss/*.scss", ["build-css"]);
+})
