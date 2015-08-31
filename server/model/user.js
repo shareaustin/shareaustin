5e60723572b3ac997d45eefeeb651754791f4d3f
dtc3ce <dtc3ce@virginia.edu>
2015-08-31 15:58:49 -0500

    test for model relations

--- server/model/user.js
+++ server/model/user.js
@@ -24,25 +24,18 @@ var User = bookshelf.Model.extend({
     .through('Item');
   },
 
-	buyerRatings: function(){
-		return this.hasMany('Rating', 'buyer_id')
-		.through('Transaction')
-	},
-
-	// sellerRatings: function (){
-	// 	return bookshelf.knex.select('u.first_name', 'i.name', 't.id', 'r.seller_rating')
-	// 	.from('users as u')
-	// 	.innerJoin('items as i', 'u.id', 'i.seller_id')
-	// 	.innerJoin('transactions as t', 't.item_id', 'i.id')
-	// 	.innerJoin('ratings as r', 'r.transaction_id', 't.id')
-	// },
-	userRatings: function () {
-		return bookshelf.knex.select('r.seller_rating', 'u.first_name')
-		.from('users as u')
-		.innerJoin('items as i', 'u.id', 'i.seller_id')
-		.innerJoin('transactions as t', 't.item_id', 'i.id')
-		.innerJoin('ratings as r', 'r.transaction_id', 't.id')
-	}
+//	buyerRatings: function(){
+//		return this.hasMany('Rating', 'buyer_id')
+//		.through('Transaction')
+//	},
+//
+//	userRatings: function () {
+//		return bookshelf.knex.select('r.seller_rating', 'u.first_name')
+//		.from('users as u')
+//		.innerJoin('items as i', 'u.id', 'i.seller_id')
+//		.innerJoin('transactions as t', 't.item_id', 'i.id')
+//		.innerJoin('ratings as r', 'r.transaction_id', 't.id')
+//	}
 
 });
 
